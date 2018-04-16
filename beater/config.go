9419258691c0eceb4be3cf2abf5e541fa7d44628
jalvz <juan.alvarez@elastic.co>
2018-04-16 10:39:32 +0200

    bumb timeouts

--- beater/config.go
+++ beater/config.go
@@ -113,8 +113,8 @@ func defaultConfig(beatVersion string) *Config {
 		MaxUnzippedSize:    50 * 1024 * 1024, // 50mb
 		MaxHeaderSize:      1 * 1024 * 1024,  // 1mb
 		ConcurrentRequests: 40,
-		ReadTimeout:        2 * time.Second,
-		WriteTimeout:       2 * time.Second,
+		ReadTimeout:        30 * time.Second,
+		WriteTimeout:       30 * time.Second,
 		ShutdownTimeout:    5 * time.Second,
 		SecretToken:        "",
 		AugmentEnabled:     true,
